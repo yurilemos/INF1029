6e4aff9c55c8e94b0819f7e13b778d33a62da029
Yuri Lemos <yuri.lemos@parafernalia.net.br>
2022-04-06 17:46:01 -0300

    final version?

--- matrix_lib.c
+++ matrix_lib.c
@@ -1,37 +1,42 @@
+// Yuri Lemos 1610893
+// Antônio Mesquita 2210269
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "matrix_lib.h"
 
 
 
-int scalar_matrix_mult(float scalar_value, Matrix* matrix){
-	unsigned long int m = matrix->height, n = matrix->width;
+int scalar_matrix_mult(float scalar_value, struct matrix* matrix){
+	unsigned long int altura = matrix->height, largura = matrix->width;
 	int i=0,j=0;
 
-	if((m%8!=0)||(n%8!=0)){
+	if((altura%8!=0)||(largura%8!=0)){
 		return 0;
 	}
 
-	for(i=0;i<m; i++){
-		for(j=0; j<n; j++){
-			matrix->rows[i*n + j] *= scalar_value;
+	for(i=0;i<altura; i++){
+		for(j=0; j<largura; j++){
+			matrix->rows[i*largura + j] *= scalar_value;
 		}
 	}
 	return 1;
 
 };
 
-int matrix_matrix_mult(Matrix *matrixA, Matrix *matrixB, Matrix *matrixC){
-	unsigned long int m = matrixA->height, q = matrixB->width, n = matrixA->width;
+int matrix_matrix_mult(struct matrix *matrixA, struct matrix *matrixB, struct matrix *matrixC){
+	unsigned long int alturaA = matrixA->height, larguraA = matrixA->width;
+	unsigned long int alturaB = matrixB->height, larguraB = matrixB->width;
 	float multiplicacao;
-	if((m%8!=0)||(q%8!=0)||(n%8!=0)||(matrixA->width!=matrixB->height)){
+	if((larguraA!=alturaB)||(alturaA%8!=0)||(larguraB%8!=0)||(larguraA%8!=0)){
 		return 0;
 	}
 
-    for(int i=0; i<m; i++){
-        for(int k=0;k<q;k++){
-            for(int j=0; j<n; j++){
-            	multiplicacao = matrixA->rows[i*matrixA->width + j] * matrixB->rows[j*matrixB->width + k];
+    for(int i=0; i<alturaA; i++){
+        for(int k=0;k<larguraB;k++){
+            for(int j=0; j<larguraA; j++){
+            	multiplicacao = matrixA->rows[i*larguraA + j] * matrixB->rows[j*larguraB + k];
             	matrixC->rows[i*matrixC->width + k] += multiplicacao;                            
             }
         }
--- matrix_lib.h
+++ matrix_lib.h
@@ -1,10 +1,10 @@
-typedef struct matrix {
+struct matrix {
 	unsigned long int height;
 	unsigned long int width;
 	float *rows;
-} Matrix;
+};
 
 
-int scalar_matrix_mult(float scalar_value, Matrix *matrix);
+int scalar_matrix_mult(float scalar_value, struct matrix *matrix);
 
-int matrix_matrix_mult(Matrix *a, Matrix *b, Matrix *c);
+int matrix_matrix_mult(struct matrix *a, struct matrix *b, struct matrix *c);
